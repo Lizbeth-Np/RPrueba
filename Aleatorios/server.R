2299398f8a2b0fb213a3154c9c4556e1ae177e71
Lizbeth-Np <lizbeth.pilamunga@epn.edu.ec>
2025-05-08 13:46:13 +0000

    cambio

--- Aleatorios/server.R
+++ Aleatorios/server.R
@@ -1,27 +1,36 @@
-
 #Funcion que genera numeros aleatorios bajo el método congruencial
- random_cong <- function(a,m,x0,n){
-   res<-numeric(n+1)
-   res[1]<- x0
-   for (k in 2:length(res)) {
-     res[k]<- (a*res[k-1]) %% m
-   }
-   return(res[-1])
- }
- #random_cong(a=5,m=7,x0=3,n=6)
+random_cong <- function(a,m,x0,n){
+  res<-numeric(n+1)
+  res[1]<- x0
+  for (k in 2:length(res)) {
+    res[k]<- (a*res[k-1]) %% m
+  }
+  return(round(res[-1]/m,6))
+}
+#random_cong(a=5,m=7,x0=3,n=6)
+
+
+conv_matrix<-function(vector,cols=10){
+  res<- rep(NA_real_,ceiling(length(vector)/cols)*cols)
+  res[1:length(vector)]<- vector
+  res<- as.data.frame(matrix(res,nrow = ceiling(length(vector)/cols),ncol = cols,byrow= TRUE))
+  colnames(res)<-paste0("Col",1:cols)
+  return (res)
+}
+conv_matrix(c(1,2,3,4,5,6,7,8),cols = 5)
 
 library(shiny)
- library(kableExtra)
+library(kableExtra)
+library(data.table)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
-   output$tabla <- function(){
-     res <- data.table(Valor=random_cong(a=input$Constante,m=input$divisor,x0=input$Semilla,n=input$num))
-     res[,n:=1:nrow(res)]
-     res<-res[,c("n","Valor"),with = FALSE]
-     kbl(res,booktabs = TRUE,escape = FALSE)%>%
-       kable_styling(full_width = FALSE,bootstrap_options = c("bordered"),font_size = 12)%>%
-       row_spec(0,background = "#1D3889",color = "#ffffff")
-   }
+  
+  output$tabla <- function(){
+    res<- conv_matrix(random_cong(a=input$Constante,m=input$divisor,x0=input$Semilla,n=input$num))
+    kbl(res,booktabs = TRUE,escape = FALSE)%>%
+    kable_styling(full_width = FALSE,bootstrap_options = c("bordered"),font_size = 12)%>%
+      row_spec(0,background = "#1D3889",color = "#ffffff")
+      
+  }
 }
--- Aleatorios/ui.R
+++ Aleatorios/ui.R
@@ -1,50 +1,72 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that genera números aleatorios
 fluidPage(
-
-    # Application title
-    titlePanel("Generación de números aleatorios-Método congruencial"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("Semilla",
-                        "Ingrese un valor incial:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-            sliderInput("divisor",
-                        "Ingrese un valor  de m:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-            sliderInput("Constante",
-                        "Ingrese un valor de a:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-            sliderInput("num",
-                        "Cantidad de numeros a generar:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            tableOutput("tabla")
-        )
+  
+  # Application title
+  titlePanel("Generación de números aleatorios - Método congruencial"),
+  
+  # Tabset para diferentes secciones
+  tabsetPanel(
+    
+    # Primera pestaña: generación de números
+    tabPanel("Numeros aleatorios",
+             sidebarLayout(
+               sidebarPanel(
+                 numericInput("Semilla",
+                              "Ingrese un valor inicial:",
+                              min = 1,
+                              max = 50,
+                              value = 30),
+                 numericInput("divisor",
+                              "Ingrese un valor de m:",
+                              min = 1,
+                              max = 50,
+                              value = 30),
+                 numericInput("Constante",
+                              "Ingrese un valor de a:",
+                              min = 1,
+                              max = 50,
+                              value = 30),
+                 numericInput("num",
+                              "Cantidad de números a generar:",
+                              min = 1,
+                              max = 50,
+                              value = 30)
+               ),
+               mainPanel(
+                 h4("Tabla de resultados- -Método Congruencial"),
+                 br(),
+                 tableOutput("tabla"),
+                 br(),
+                 h4("Tabla de resultados -Método Congruencial MIxto "),
+                 br(),
+                 br(), #aqui va la segunda tabla
+                 br(),
+                 h2("Gráfico:"),
+                 fluidRow(
+                   column(2,
+                          numericInput("barras", "Número de barras:", value = 10,min = 2,max = 20),
+                          ),
+                   column(5,
+                          h5("Método 1")),
+                   column(5,
+                          h5("Método 2"))
+                 )
+                 
+               )
+             )
+    ),
+    
+    # Segunda pestaña: Resultados
+    tabPanel("Resultados",
+             h1("Aquí se muestran los resultados")
+    ),
+    
+    # Tercera pestaña: Integrales
+    tabPanel("Integrales",
+             h1("Estimación de integrales")
     )
+    
+  ) # fin tabsetPanel
 )
-
-
